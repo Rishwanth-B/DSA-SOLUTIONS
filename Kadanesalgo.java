0a2836e31ecc8eaba4c1c882823882dc78698902
Rishwanth-B <rishwanthb71@gmail.com>
2025-07-04 14:27:28 +0530

    Update Kadanesalgo.java

---
+++ Kadanesalgo.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=0;
+        int sum=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            max+=nums[i];
+         sum=Math.max(sum,max);
+         if(max<0){
+            max=0;
+         }
+        }
+        return sum;
+    }
+}
